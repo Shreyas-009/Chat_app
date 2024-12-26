0b82ee587fd4a204a1e28d62851b22508ab1c2d0
Shreyas <shreyastungar762@gmail.com>
2024-12-26 20:48:01 +0530

    unnecessary code removed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import NavBar from "./pages/NavBar";
+import React from 'react'
 import Routing from "./Routing";
 import toast, { Toaster } from "react-hot-toast";
 
@@ -9,7 +8,6 @@ const App = () => {
     <>
       <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
       <div className="w-full h-screen bg-zinc-900 text-white">
-        {/* <NavBar /> */}
         <Routing />
       </div>
     </>
